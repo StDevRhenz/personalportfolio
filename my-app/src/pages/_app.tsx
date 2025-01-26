04d0513c91e6f57133294ed98c03a9b659a6a569
STRhenz <rhenzganotice28@gmail.com>
2025-01-26 09:17:19 +0800

    forDeployment

--- my-app/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// filepath: /c:/Users/Ganotice/OneDrive/Desktop/OWN WEBSITE/Portfolio/personalPortfolio2025/PersonalPortfolio-1/my-app/src/pages/_app.tsx
-import '../styles/globals.css';
-import { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- my-practicereact/src/App.js
+++ my-practicereact/src/App.js
@@ -1,31 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleButtonClick = () => {
-    alert(`You entered: ${inputValue}`);
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>My React App</h1>
-        <input
-        type = "Text"
-        value={inputValue}
-        onChange={handleChange}
-        placeholder="Enter text here"
-        />
-
-        <button onClick={handleButtonClick}>Submit</button>
-      </header>
+      <h1>Rhenz Ganotice</h1>
     </div>
   );
   
