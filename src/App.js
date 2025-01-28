cf0ce8f3afe477e251a601f1fbdd306f9f264425
STRhenz <rhenzganotice28@gmail.com>
2025-01-29 04:35:09 +0800

    2nd  publish weebsite

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <center>
-          <p>The project is currently underway....</p>
+          <p>Still in progress....</p>
           <a href="https://www.umak.edu.ph" target="_blank" rel="noopener noreferrer" className='linktag'>
             <p>Hi im Rhenz from Bachelor of Science Computer Science Major in Application Development - University of Makati</p>
           </a>  
