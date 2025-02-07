d60315fc3db5ab440ade97028939c26f66fe5bc3
STRhenz <rhenzganotice28@gmail.com>
2025-02-07 12:35:38 +0800

    updated version

--- my-app/next.config.ts
+++
@@ -1,4 +0,0 @@
-// filepath: /c:/Users/Ganotice/OneDrive/Desktop/OWN WEBSITE/Portfolio/personalPortfolio2025/PersonalPortfolio-1/my-app/next.config.js
-module.exports = {
-  reactStrictMode: true,
-};
--- my-practicereact/src/App.js
+++ my-practicereact/src/App.js
@@ -1,34 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
 
-function App() {
-  const [inputValue, setInputValue] = useState('');
 
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleButtonClick = () => {
-    alert(`You entered: ${inputValue}`);
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My React App</h1>
-        <input
-        type = "Text"
-        value={inputValue}
-        onChange={handleChange}
-        placeholder="Enter text here"
-        />
-
-        <button onClick={handleButtonClick}>Submit</button>
-      </header>
-    </div>
-  );
-  
+function Square(){
+  return <button className="square">X</button>;
 }
 
-export default App;
+export default Square;
