d5761550955029a7004596ca606565cc861e39d7
STRhenz <rhenzganotice28@gmail.com>
2025-02-13 00:24:00 +0800

    Hello

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Card from './Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
+      <h1>3D Card Design</h1>
+      <Card />
     </div>
   );
 }
