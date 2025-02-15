7bc80b5425af1a4ab776725db957682113ec8c9d
STRhenz <rhenzganotice28@gmail.com>
2025-02-15 10:30:22 +0800

    newver

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,11 @@ const About = () => {
     <section className="about">
       <h2>About Me</h2>
       <p>
-        Hi, I'm Rhenz, a web developer with a passion for creating beautiful and functional websites.
+        Hi, I'm a second-year Computer Science student at  
+        <a href="https://www.umak.edu.ph/"  style={{ textDecoration: "none", color: "#bb86fc" }}> University of Makati</a>, majoring in Application Development.  
+        As a backend developer, I specialize in building efficient, scalable, and secure systems that power web applications.  
+        I thrive on solving complex problems, optimizing performance, and ensuring seamless data flow between the frontend and backend.  
+        Always eager to learn new technologies and enhance my skills in backend development!
       </p>
     </section>
   );
