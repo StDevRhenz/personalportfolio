bc566820e4e2fef922b9222903d669d9189d187c
STRhenz <rhenzganotice28@gmail.com>
2025-06-03 17:17:26 +0800

    ads added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,28 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Head from "next/head";
 //import SiteNotice from "./components/SiteNotice";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white dark:bg-gray-900">
-      <Header />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <About />
-        <Projects />
-        <Contact />
-      </div>
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7172996117171177"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <main className="min-h-screen bg-white dark:bg-gray-900">
+        <Header />
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+          <About />
+          <Projects />
+          <Contact />
+        </div>
+        <Footer />
+      </main>
+    </>
   );
 }
